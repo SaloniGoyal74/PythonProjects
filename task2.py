0d7996b9e083fd643ad0d612dfc5db9ba9162006
SaloniGoyal74 <126331453+SaloniGoyal74@users.noreply.github.com>
2023-03-01 01:11:08 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,145 @@
+#LGMVIP TASK 2 
+# SALONI GOYAL
+# STOCK MARKET PREDICTION AND FORECASTING USING STACKED LSTM
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+df =pd.read_csv('https://raw.githubusercontent.com/mwitiderrick/stockprice/master/NSE-TATAGLOBAL.csv')
+df=df.iloc[::-1]
+df.head()
+df.tail()
+df.shape
+df.columns
+df.info()
+df.describe()
+#Data Preprocessing
+df.isnull().sum()
+duplicates= df.duplicated()
+duplicates.value_counts() # no duplicates present
+plt.figure(figsize=(6,6))
+sns.heatmap(df.corr(), annot=True)
+df_high=df.reset_index()['High']
+plt.plot(df_high)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range = (0,1))
+df_high = scaler.fit_transform(np.array(df_high).reshape(-1,1))
+df_high.shape
+df_high
+#Split the data into train and test split
+training_size = int(len(df_high) * 0.75)
+test_size = len(df_high) - training_size
+train_data,test_data = df_high[0:training_size,:], df_high[training_size:len(df_high),:1]
+training_size, test_size
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a = dataset[i:(i+time_step), 0]  
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+time_step = 100
+x_train, y_train = create_dataset(train_data, time_step)
+x_test, y_test = create_dataset(test_data, time_step)
+#Reshape the input to be [samples, time steps, features] which is the requirement of LSTM
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+print(x_train.shape), print(y_train.shape)
+print(x_test.shape), print(y_test.shape)
+import tensorflow as tf
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import LSTM
+### Create the Stacked LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences = True, input_shape = (100,1)))
+model.add(LSTM(50, return_sequences = True))
+model.add(LSTM(50))
+model.add(Dense(1))
+model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+model.summary()
+model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs = 100, batch_size = 64, verbose = 1)
+#predictions
+#Lets predict and check performance metrics
+train_predict = model.predict(x_train)
+test_predict = model.predict(x_test)
+#Transform back to original form
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+#Calculate RMSE performance metrics
+import math
+from sklearn.metrics import mean_squared_error
+math.sqrt(mean_squared_error(y_train, train_predict))
+#Test Data RMSE
+math.sqrt(mean_squared_error(y_test, test_predict))
+#Plotting
+#Shift train prediction for plotting
+look_back = 100
+trainPredictPlot = np.empty_like(df_high)
+trainPredictPlot[:,:] = np.nan
+trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
+
+#Shift test prediction for plotting
+testPredictPlot = np.empty_like(df_high)
+testPredictPlot[:,:] = np.nan
+testPredictPlot[len(train_predict) + (look_back * 2)+1:len(df_high) - 1, :] = test_predict
+#Plot baseline and predictions
+plt.plot(scaler.inverse_transform(df_high))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+
+print("Green indicates the Predicted Data")
+print("Blue indicates the Complete Data")
+print("Orange indicates the Train Data")
+#Predict the next 28 days Stock Price
+len(test_data), x_test.shape
+x_input = test_data[409:].reshape(1,-1)
+x_input.shape
+temp_input = list(x_input)
+temp_input = temp_input[0].tolist()
+
+lst_output=[]
+n_steps=100
+nextNumberOfDays = 28
+i=0
+
+while(i<nextNumberOfDays):
+    
+    if(len(temp_input)>100):
+        x_input=np.array(temp_input[1:])
+        print("{} day input {}".format(i,x_input))
+        x_input=x_input.reshape(1,-1)
+        x_input = x_input.reshape((1, n_steps, 1))
+        yhat = model.predict(x_input, verbose=0)
+        print("{} day output {}".format(i,yhat))
+        temp_input.extend(yhat[0].tolist())
+        temp_input=temp_input[1:]
+        lst_output.extend(yhat.tolist())
+        i=i+1
+    else:
+        x_input = x_input.reshape((1, n_steps,1))
+        yhat = model.predict(x_input, verbose=0)
+        print(yhat[0])
+        temp_input.extend(yhat[0].tolist())
+        print(len(temp_input))
+        lst_output.extend(yhat.tolist())
+        i=i+1
+    
+print(lst_output)
+day_new = np.arange(1,101)
+day_pred = np.arange(101,129)
+day_new.shape
+day_pred.shape
+len(df_high)
+plt.plot(day_new, scaler.inverse_transform(df_high[1935:]))
+plt.plot(day_pred, scaler.inverse_transform(lst_output))
+data_new = df_high.tolist()
+data_new.extend(lst_output)
+plt.plot(data_new[2000:])
+data_new =scaler.inverse_transform(data_new).tolist()
+plt.plot(data_new)
